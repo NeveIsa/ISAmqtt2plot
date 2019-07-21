683383b604babdbf86f7e8436fd8c901a10dc7b1
sampad <smpdmohanty@gmail.com>
2019-07-21 19:21:55 +0530

    auto commit by sampad

--- tools/pub.sh
+++ tools/pub.sh
@@ -4,5 +4,6 @@ do
 rand=`python -c 'from random import randint;import math;print randint(1,10000)'`
 echo $rand
 mosquitto_pub -u sam -P pad -t hello/world -m "$rand"
+#mosquitto_pub -t hello/world -m "$rand"
 #sleep 0.01
 done
